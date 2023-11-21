1098e8899bf47d30b23888ef996dd79ad98f4717
Sange Sherpa <sherpasange1215@gmail.com>
2023-11-21 10:48:17 +0545

    test

--- NewList/Views/RegisterView.swift
+++ NewList/Views/RegisterView.swift
@@ -10,11 +10,6 @@ import SwiftUI
 struct RegisterView: View {
     @StateObject var viewmodel = RegisterViewModel()
     
-<<<<<<< HEAD
-    @StateObject var viewModel = RegisterViewModel()
-    
-=======
->>>>>>> d0a8cb5 (test)
     var body: some View {
         VStack {
             // Header
@@ -25,16 +20,6 @@ struct RegisterView: View {
             
             // Login Form
             Form {
-<<<<<<< HEAD
-                TextField("Full Name", text: $viewModel.name)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                
-                TextField("Email", text: $viewModel.email)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .autocapitalization(.none)
-                
-                SecureField("Password", text: $viewModel.password)
-=======
                 TextField("Full Name", text: $viewmodel.name)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                 
@@ -43,14 +28,12 @@ struct RegisterView: View {
                     .autocapitalization(.none)
                 
                 SecureField("Password", text: $viewmodel.password)
->>>>>>> bf2e3c0 (registered)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                 
                 CustomButtonView(
                     title: "Register",
                     backgroundColor: .blue) {
                         viewmodel.register()
-                        MainView()
                     }
                     .padding()
             }
